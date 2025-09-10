4c2998a362e24b77a91e33e048eb38ba3f50b693
jomahabibullaev-ctrl <jomahabibullaev@gmail.com>
2025-09-10 10:35:42 +0600

    Первоначальные файлы для проекта MakeCode

---
+++ main.ts
@@ -0,0 +1,27 @@
+info.onScore(100, function () {
+	
+})
+let mySprite = 0
+mySprite += 1
+game.onUpdate(function () {
+    music.play(music.melodyPlayable(music.baDing), music.PlaybackMode.UntilDone)
+    scene.setBackgroundImage(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `)
+    mySprite = 0
+})
